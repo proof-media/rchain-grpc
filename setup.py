e284b2bf0e33c4cc2a3ebbeb3e5b60f3033976dc
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2018-10-05 14:23:43 +0200

    pants compatibility

--- setup.py
+++ setup.py
@@ -42,6 +42,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/proof-media/rchain_grpc',
-    version='0.0.1',
-    zip_safe=False,
+    version='0.0.2',
+    zip_safe=True,
 )
