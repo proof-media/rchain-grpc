848eb41caeb590e422044f4dc88ebb45dea7c1b0
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2018-10-12 12:58:14 +0200

    0.0.7

--- setup.py
+++ setup.py
@@ -42,6 +42,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/proof-media/rchain_grpc',
-    version='0.0.6',
+    version='0.0.7',
     zip_safe=True,
 )
