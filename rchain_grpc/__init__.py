adc24d1dcb7887f2f19c3efbeae1f2dfe7eb58b2
Paolo <paolo@proofmedia.io>
2018-10-15 17:42:03 +0200

    wrong fix

--- rchain_grpc/__init__.py
+++ rchain_grpc/__init__.py
@@ -1,13 +1,14 @@
 # hack needed becouse protc use absolute imports
 import os
 import sys
-import CasperMessage_pb2  # isort:skip
-import CasperMessage_pb2_grpc  # isort:skip
-import RhoTypes_pb2  # isort:skip
-import RhoTypes_pb2_grpc  # isort:skip
 
 ROOT = os.path.dirname(__file__)
 GENERATED = os.path.join(ROOT, 'generated')
 sys.path.append(GENERATED)
+
+import CasperMessage_pb2  # isort:skip
+import CasperMessage_pb2_grpc  # isort:skip
+import RhoTypes_pb2  # isort:skip
+import RhoTypes_pb2_grpc  # isort:skip
 
 __version__ = '0.0.9'
