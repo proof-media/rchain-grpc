dde7148f24255660ae06dafdd2f9075db912104a
Paolo <paolo@proofmedia.io>
2018-10-15 12:22:02 +0200

    bump 0.0.8

--- setup.py
+++ setup.py
@@ -42,6 +42,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/proof-media/rchain_grpc',
-    version='0.0.7',
+    version='0.0.8',
     zip_safe=True,
 )
