620d76dbd121e0ccee8543d11ebce2a7ed2cda2a
Mateusz Probachta <beetleman@users.noreply.github.com>
2018-11-02 14:11:42 +0100

    handle all types at once using `utils.register_many` (#20)

--- rchain_grpc/_grpc_containers.py
+++ rchain_grpc/_grpc_containers.py
@@ -1,16 +1,25 @@
 # unify difference between python 3.7 and 3.6
 import sys
+from typing import Union
+
+from google.protobuf.pyext._message import (RepeatedCompositeContainer,
+                                            RepeatedScalarContainer)
+
+container_classes = [RepeatedCompositeContainer, RepeatedScalarContainer]
+IContainer = Union[RepeatedCompositeContainer, RepeatedScalarContainer]
 
 if sys.version_info >= (3, 7):
     from google.protobuf.internal.containers import (
-        RepeatedCompositeFieldContainer as _Composite,
-        RepeatedScalarFieldContainer as _Scalar,
-    )
-else:
-    from google.protobuf.pyext._message import (
-        RepeatedCompositeContainer as _Composite,
-        RepeatedScalarContainer as _Scalar,
+        RepeatedCompositeFieldContainer,
+        RepeatedScalarFieldContainer,
     )
 
-RepeatedCompositeFieldContainer = _Composite
-RepeatedScalarFieldContainer = _Scalar
+    container_classes.extend(
+        (RepeatedCompositeFieldContainer, RepeatedScalarFieldContainer)
+    )
+    IContainer = Union[
+        RepeatedCompositeContainer,
+        RepeatedScalarContainer,
+        RepeatedCompositeFieldContainer,
+        RepeatedScalarFieldContainer,
+    ]
