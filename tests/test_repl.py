c4b133d8bc12c532b48bc0b09bff48a6ae0b7135
Paolo D <pdonorio@users.noreply.github.com>
2018-11-19 10:05:41 +0100

    Docs/adding repl example (#32)
    * Moving repl parser out of tests to be used from anyone
    
    * Adding docs for eval/repl

--- tests/test_repl.py
+++ tests/test_repl.py
@@ -1,24 +1,10 @@
-import re
 import pytest
 from rchain_grpc import repl
 
 
-def repl_parser(returned):
-    output = returned.get('output', '')
-    regex = re.compile(r"deployment cost: ([^\n]+)", re.IGNORECASE)
-    m = regex.match(output)
-    if m:
-        costs = m.groups()[0]
-    else:
-        costs = None
-    regex = re.compile(r"^.*storage contents\:", re.IGNORECASE)
-    tuple_space = regex.sub('', output.replace('\n', ''))
-    return tuple_space, costs
-
-
 def verify_repl_output(repl_output):
     assert isinstance(repl_output, dict)
-    tuple_space, costs = repl_parser(repl_output)
+    tuple_space, costs = repl.output_parser(repl_output)
     assert 'Cost' in costs
     for keyword in ['Unforgeable', 'for', 'Nil']:
         keyword in tuple_space
