393f1b4b917d6074723687246455010a562e5850
Paolo <paolo@proofmedia.io>
2018-11-19 09:46:57 +0100

    Moving repl parser out of tests to be used from anyone

--- rchain_grpc/repl.py
+++ rchain_grpc/repl.py
@@ -1,3 +1,4 @@
+import re
 from . import rho_types
 from .generated import repl_pb2_grpc
 from .generated.repl_pb2 import CmdRequest, EvalRequest
@@ -5,6 +6,19 @@ from .utils import Connection, create_connection_builder
 
 # NOTE: default port for REPL is 40402
 create_connection = create_connection_builder(repl_pb2_grpc.ReplStub)
+
+
+def output_parser(returned):
+    output = returned.get('output', '')
+    regex = re.compile(r"deployment cost: ([^\n]+)", re.IGNORECASE)
+    m = regex.match(output)
+    if m:
+        costs = m.groups()[0]
+    else:
+        costs = None
+    regex = re.compile(r"^.*storage contents\:", re.IGNORECASE)
+    tuple_space = regex.sub('', output.replace('\n', ''))
+    return tuple_space, costs
 
 
 def run(connection: Connection, line: str) -> dict:
